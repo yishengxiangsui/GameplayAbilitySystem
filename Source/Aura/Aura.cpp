386bec4bd6f796074d91c8efb7f59dd63d48c1ed
yishengxiangsui <1506160036@qq.com>
2023-10-19 11:22:08 +0800

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// This Project Made By Yuan Xiaodong.
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// This Project Made By Yuan Xiaodong.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
